27f362f79685dcf6d9e91eee4dc599eea1b39353
faiyuching <faiyuching@magic.local>
2021-04-12 20:31:47 +0800

    update

--- src/routes/forum/post/index.ts
+++ src/routes/forum/post/index.ts
@@ -3,6 +3,7 @@ import { requireAuth } from '@sgtickets/common';
 import { Post } from '../../../models/forum/post';
 import { User } from '../../../models/user';
 import { Responses } from '../../../models/response/responses';
+import { Op } from "sequelize"
 
 const router = express.Router();
 
@@ -10,15 +11,25 @@ router.get('/forum/post',
     // requireAuth,
     async (req: Request, res: Response) => {
 
-        const { tag } = req.query
+        const { tag, search } = req.query
 
         let posts
+
 
         if (tag) {
             posts = await Post.findAll({
                 include: [User],
                 order: [['created_at', 'DESC']],
                 where: { tag: tag },
+            });
+        } else if (search) {
+            const content = {
+                [Op.like]: '%' + search + '%'
+            }
+            posts = await Post.findAll({
+                include: [User],
+                order: [['created_at', 'DESC']],
+                where: { content: content },
             });
         } else {
             posts = await Post.findAll({
