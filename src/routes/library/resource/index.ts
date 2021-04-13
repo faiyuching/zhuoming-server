9a7d672d8f90931308ad5d0c8b705921df0392fc
faiyuching <faiyuching@magic.local>
2021-04-14 03:25:40 +0800

    update

--- src/routes/library/resource/index.ts
+++ src/routes/library/resource/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@sgtickets/common';
+import { requireAuth, NotFoundError } from '@sgtickets/common';
 import { Resource } from '../../../models/library/resource';
 import { Filetype } from '../../../models/library/filetype';
 import { Category } from '../../../models/library/category';
@@ -9,11 +9,38 @@ const router = express.Router();
 router.get('/resources',
     // requireAuth,
     async (req: Request, res: Response) => {
+        const { filetype_name, category_name } = req.query
+        let resources
 
-        const resources = await Resource.findAll({
-            include: [Filetype, Category],
-            order: [['created_at', 'DESC']],
-        });
+        if (filetype_name !== 'undefined' && category_name !== 'undefined') {
+            const filetype = await Filetype.findOne({ where: { filetype_name: filetype_name } });
+            const category = await Category.findOne({ where: { category_name: category_name } });
+            resources = await Resource.findAll({
+                include: [Filetype, Category],
+                order: [['created_at', 'DESC']],
+                where: { filetype_id: filetype?.filetype_id, category_id: category?.category_id }
+            });
+        } else if (filetype_name === 'undefined' && category_name !== 'undefined') {
+            const category = await Category.findOne({ where: { category_name: category_name } });
+            resources = await Resource.findAll({
+                include: [Filetype, Category],
+                order: [['created_at', 'DESC']],
+                where: { category_id: category?.category_id }
+            });
+        } else if (filetype_name !== 'undefined' && category_name === 'undefined') {
+            const filetype = await Filetype.findOne({ where: { filetype_name: filetype_name } });
+            resources = await Resource.findAll({
+                include: [Filetype, Category],
+                order: [['created_at', 'DESC']],
+                where: { filetype_id: filetype?.filetype_id }
+            });
+        } else if (filetype_name === 'undefined' && category_name === 'undefined') {
+            resources = await Resource.findAll({
+                include: [Filetype, Category],
+                order: [['created_at', 'DESC']],
+            });
+        }
+
 
         res.send(resources);
 
--- src/routes/library/topic/index.ts
+++ src/routes/library/topic/index.ts
@@ -2,19 +2,29 @@ import express, { Request, Response } from 'express';
 import { requireAuth } from '@sgtickets/common';
 import { Topic } from '../../../models/library/topic';
 import { Category } from '../../../models/library/category';
-import { User } from '../../../models/user';
-import { Responses } from '../../../models/response/responses';
 
 const router = express.Router();
 
 router.get('/topics',
     // requireAuth,
     async (req: Request, res: Response) => {
+        const { category_name } = req.query
+        let topics
 
-        const topics = await Topic.findAll({
-            include: [Category],
-            order: [['created_at', 'ASC']],
-        });
+        if(!category_name || category_name === 'undefined'){
+            topics = await Topic.findAll({
+                include: [Category],
+                order: [['created_at', 'ASC']],
+            });
+        }else{
+            const category = await Category.findOne({ where: { category_name: category_name } });
+            topics = await Topic.findAll({
+                include: [Category],
+                order: [['created_at', 'ASC']],
+                where: { category_id: category?.category_id }
+            });
+        }
+
         res.send(topics);
 
     });
